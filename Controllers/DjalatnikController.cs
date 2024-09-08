35813d4ae83d7e7b3a3899fd4348aea0a8b76e58
Nikola Milić <nikk.mil@gmail.com>
2024-09-08 12:46:16 +0200

    Dependency Injection

--- Controllers/DjalatnikController.cs
+++ Controllers/DjalatnikController.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CsharpApi_EvidencijaRada.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CsharpApi_EvidencijaRada.Controllers
 {
     public class DjalatnikController:ControllerBase
     {
+
+        private readonly EvidencijaContext _context;
+
+
+        public DjalatnikController(EvidencijaContext context)
+        {
+            _context = context;
+        }
+
+    
+
     }
 }
