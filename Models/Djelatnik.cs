67819783780a453887a0668a0ab304a1bf6f320e
Nikola Milić <nikk.mil@gmail.com>
2024-09-08 12:37:04 +0200

    Create Djelatnik.cs

---
+++ Models/Djelatnik.cs
@@ -0,0 +1,13 @@
+﻿namespace CsharpApi_EvidencijaRada.Models
+{
+    public class Djelatnik:Entitet
+    {
+
+        public string? Ime { get; set; }
+
+        public string? Prezime { get; set; }
+
+        public string? Email { get; set; }
+
+    }
+}
