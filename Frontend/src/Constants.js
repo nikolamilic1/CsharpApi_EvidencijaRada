b228f1b5079399da320a39f98e9c28afb4f3331e
nikolamilic1 <57691651+nikolamilic1@users.noreply.github.com>
2024-09-19 18:54:40 +0200

    Update Constants.js

--- Frontend/src/Constants.js
+++ Frontend/src/Constants.js
@@ -1,8 +1,12 @@
 export const RoutesNames = {
 
 HOME: '/',
-PROJEKT_PREGLED: '/Projekt'
+PROJEKT_PREGLED: '/Projekt',
+PROJEKT_NOVI: '/projekti/dodaj',
+PROJEKT_PROMJENA: '/projekti/:sifra'
+
 
 };
 
 export const BACKEND_URL = 'https://nikolamilic1-001-site1.htempurl.com/api/v1';
+
