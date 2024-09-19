b82d97cee99bcc8dd317978ee3bd7b4c459a340d
nikolamilic1 <57691651+nikolamilic1@users.noreply.github.com>
2024-09-19 18:54:49 +0200

    Update ProjektSevice.js

--- Frontend/src/services/ProjektSevice.js
+++ Frontend/src/services/ProjektSevice.js
@@ -10,6 +10,53 @@ async function get() {
     .catch((e)=>{console.error(e)})
 }
 
+async function getBySifra(sifra){
+    return await HttpService.get('/Projekt/' + sifra)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        return {greska: true, poruka: 'Ne postoji projekt!'}
+    })
+}
+
+async function obrisi(sifra) {
+    return await HttpService.delete('/Projekt/' + sifra)
+    .then((odgovor)=>{
+        //console.log(odgovor);
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        //console.log(e);
+        return {greska: true, poruka: 'Projekt se ne može obrisati!'}
+    })
+}
+
+async function dodaj(projekt) {
+    return await HttpService.post('/Projekt',projekt)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        return {greska: true, poruka: 'Projekt se ne može dodati!'}
+    })
+}
+
+async function promjena(sifra,projekt) {
+    return await HttpService.put('/Projekt/' + sifra,projekt)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        return {greska: true, poruka: 'Projekt se ne može promjeniti!'}
+    })
+}
+
+
 export default{
-    get
+    get,
+    getBySifra,
+    obrisi,
+    dodaj,
+    promjena
 }
