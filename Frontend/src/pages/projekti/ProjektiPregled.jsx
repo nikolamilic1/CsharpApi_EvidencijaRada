8d70b0724132a46b46d3edb1c6424178c2659524
Nikola Milić <nikk.mil@gmail.com>
2024-09-20 20:59:24 +0200

    Ruta ProjektPregled

---
+++ Frontend/src/pages/projekti/ProjektiPregled.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+
+
+export default function ProjektiPregled(){
+
+
+
+    return(
+        <Container>
+            Pregled projekata
+        </Container>
+    )
+}
