07f25ae568af0e648def75f59c1daed3b78adbe3
Nikola Milić <nikk.mil@gmail.com>
2024-09-20 20:59:20 +0200

    Ruta ProjektPregled

---
+++ Frontend/src/pages/Pocetna.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+
+
+export default function Pocetna(){
+    return(
+        <Container>
+            Ovdje ćemo kasnije nešto dodati.
+        </Container>
+    )
+
+
+}
