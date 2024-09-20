de4a0436ea867c5d6acc82e7aaf1da5680dd35bd
Nikola Milić <nikk.mil@gmail.com>
2024-09-20 20:58:58 +0200

    Ruta ProjektPregled

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import NavBarEvidencija from './components/NavBarEvidencija'
+import { Route, Routes } from 'react-router-dom'
+import { RoutesNames } from './Constants'
+import Pocetna from './pages/Pocetna'
+import ProjektiPregled from './pages/projekti/ProjektiPregled'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBarEvidencija />
+      <Routes>
+        <Route path={RoutesNames.HOME} element={<Pocetna/>} />
+
+        <Route path={RoutesNames.PROJEKT_PREGLED} element={<ProjektiPregled />} />
+
+        
+      </Routes>
     </>
   )
 }
