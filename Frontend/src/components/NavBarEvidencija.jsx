c982c9cb215fe47ffc3d2913c087bca77892b0a7
Nikola Milić <nikk.mil@gmail.com>
2024-09-20 20:59:02 +0200

    Ruta ProjektPregled

---
+++ Frontend/src/components/NavBarEvidencija.jsx
@@ -0,0 +1,38 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { RoutesNames } from '../Constants';
+
+export default function NavBarEvidencija() {
+
+    const navigate = useNavigate();
+
+    return(
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Evidencija rada</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link onClick={()=>navigate(RoutesNames.HOME)}>Početna</Nav.Link>
+            <Nav.Link href="https://nikolamilic1-001-site1.htempurl.com/swagger/index.html" target='_blank'>Swagger</Nav.Link>
+            <NavDropdown title="Programi" id="basic-nav-dropdown">
+              <NavDropdown.Item onClick={()=>navigate(RoutesNames.PROJEKT_PREGLED)}>Projekti</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+    
+}
