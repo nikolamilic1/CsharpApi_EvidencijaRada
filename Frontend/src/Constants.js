3c97dd3a57b39d99b9cfbe6a55d914cdd280ecb5
Nikola Milić <nikk.mil@gmail.com>
2024-09-21 16:29:29 +0200

    Priprema Dodaj Novi Projekt Frontend

---
+++ Frontend/src/Constants.js
@@ -0,0 +1,8 @@
+export const RoutesNames = {
+
+HOME: '/',
+PROJEKT_PREGLED: '/projekti',
+PROJEKT_NOVI: '/projekti/dodaj'
+};
+
+export const BECKEND_URL = 'https://nikolamilic1-001-site1.htempurl.com/api/v1';
