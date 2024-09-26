caf7471304dbd172b74d77d0240acd915b1f5d37
Raguž Lea <57691651+nikolamilic1@users.noreply.github.com>
2024-09-26 18:48:21 +0200

    Definiranje mappera za Projekt
    Konfiguracija Automapera i definiranje mapera za Projekt

--- Mapping/EvidencijaMappingProfile.cs
+++ Mapping/EvidencijaMappingProfile.cs
@@ -1,6 +1,18 @@
-﻿namespace CsharpApi_EvidencijaRada.Mapping
+﻿using AutoMapper;
+using CsharpApi_EvidencijaRada.Models;
+using CsharpApi_EvidencijaRada.Models.DTO;
+
+namespace CsharpApi_EvidencijaRada.Mapping
 {
-    public class EvidencijaMappingProfile
+    public class EvidencijaMappingProfile:Profile
     {
+        public EvidencijaMappingProfile()
+        {
+            //kreiranje mapiranja: izvor, odredište
+            CreateMap<Projekt, ProjektDTORead>();
+            CreateMap<ProjektDTORead, Projekt>();
+            CreateMap<ProjektDTOInsertUpdate, Projekt>();
+        }
+            
     }
 }
