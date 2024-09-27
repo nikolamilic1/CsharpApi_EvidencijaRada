7cf9aa0da6f9529fd28334cce7d643dd190e0494
Nikola Milić <nikk.mil@gmail.com>
2024-09-27 14:00:37 +0200

    Update CsharpApi_EvidencijaRada.csproj

---
+++ Controllers/EvidencijaController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CsharpApi_EvidencijaRada.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsharpApi_EvidencijaRada.Controllers
+{
+    public abstract class EvidencijaController:ControllerBase
+    {
+
+        //dependecy injection
+        // 1. definiranje privatnog svojstva
+        
+        protected readonly EvidencijaContext _context;
+
+        protected readonly IMapper _mapper;
+
+        //dependecy injection
+        // 2. proslijđivanje instance kroz konstruktor
+        public EvidencijaController(EvidencijaContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+    }
+}
