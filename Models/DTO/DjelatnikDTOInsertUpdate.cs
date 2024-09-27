824e94fa6b91f9df9ef69990b46cf89168698cf0
Nikola Milić <nikk.mil@gmail.com>
2024-09-27 17:09:24 +0200

    DjelatanikDTOInserUpdate

--- Models/DTO/DjelatnikDTOInsertUpdate.cs
+++ Models/DTO/DjelatnikDTOInsertUpdate.cs
@@ -1,6 +1,14 @@
-﻿namespace CsharpApi_EvidencijaRada.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsharpApi_EvidencijaRada.Models.DTO
 {
-    public class DjelatnikDTOInsertUpdate
-    {
-    }
+    public record DjelatnikDTOInsertUpdate(
+        [Required(ErrorMessage = "Ime obavezno")]
+        string? Ime,
+        [Required(ErrorMessage = "Prezime obavezno")]
+        string? Prezime,
+        [Required(ErrorMessage = "Email obavezno")]
+        [EmailAddress(ErrorMessage = "Email nije dobrog formata")]
+        string? Email);
+
 }
