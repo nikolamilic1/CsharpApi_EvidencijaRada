0b6d1ee5405c2bdbadb3e98418375eaefdd8c34d
Nikola Milić <nikk.mil@gmail.com>
2024-09-28 20:40:47 +0200

    Update DjelatnikService.js

--- Frontend/src/services/DjelatnikService.js
+++ Frontend/src/services/DjelatnikService.js
@@ -11,9 +11,68 @@ async function get() {
 }
 
 async function getBySifra(sifra) {
-    
+    return await HttpService.get('/Djelatnik/' + sifra)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch(()=>{
+        return {greska: true, poruka: 'Ne postoji djelatnik!'}
+    })
 }
 
-async function name(params) {
-    
+async function obrisi(sifra) {
+    return await HttpService.delete('/Djelatnik/' + sifra)
+    .then((odgovor)=>{
+        //console.log(odgovor);
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch(()=>{
+        return {greska: true, poruka: 'Djelatnik se ne može obrisati!'}
+    })
+}
+
+async function dodaj(Djelatnik) {
+    return await HttpService.post('/Djelatnik',Djelatnik)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        switch (e.status) {
+            case 400:
+                let poruke='';
+                for(const kljuc in e.response.data.errors){
+                    poruke += kljuc + ': ' + e.response.data.errors[kljuc][0] + '\n';
+                }
+                return {greska: true, poruka: poruke}
+            default:
+                return {greska: true, poruka: 'Djelatnik se ne može dodati!'}
+        }
+    })
+}
+
+async function promjena(sifra,Djelatnik) {
+    return await HttpService.put('/Djelatnik/' + sifra,Djelatnik)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch(()=>{
+        switch (e.status) {
+            case 400:
+                let poruke='';
+                for(const kljuc in e.response.data.errors){
+                    poruke += kljuc + ': ' + e.response.data.errors[kljuc][0] + '\n';
+                }
+                return {greska: true, poruka: poruke}
+            default:
+                return {greska: true, poruka: 'Djelatnik se ne može promjeniti!'}
+        }
+    })
+}
+
+export default{
+    get,
+    getBySifra,
+    obrisi,
+    dodaj,
+    promjena
 }
