75c564c8e95ecdda61452fcff59102212c792654
Nikola Milić <nikk.mil@gmail.com>
2024-09-28 20:02:09 +0200

    Update Constants.js

--- Frontend/src/Constants.js
+++ Frontend/src/Constants.js
@@ -3,7 +3,11 @@ export const RoutesNames = {
 HOME: '/',
 PROJEKT_PREGLED: '/projekti',
 PROJEKT_NOVI: '/projekti/dodaj',
-PROJEKT_PROMJENA: '/projekti/:sifra'
+PROJEKT_PROMJENA: '/projekti/:sifra',
+
+DJELATNIK_PREGLED: '/djelatnik',
+DJELATNIK_NOVI: '/djelatnik/dodaj',
+DJELATNIK_PROMJENA: '/djelatnik/:sifra'
 };
 
 export const BECKEND_URL = 'https://nikolamilic1-001-site1.htempurl.com/api/v1';
