a075725e5e668fe4718b138c6b6e0b2c8d26fc67
Nikola Milić <nikk.mil@gmail.com>
2024-09-28 21:20:13 +0200

    Update DjeltniciPromjena.jsx

--- Frontend/src/pages/djelatnici/DjeltniciPromjena.jsx
+++ Frontend/src/pages/djelatnici/DjeltniciPromjena.jsx
@@ -46,6 +46,45 @@ export default function DjelatniciPromjena(){
     }
 
 
+    return(
+        <>
+            Promjena Polaznika
+            
+            <Form onSubmit={obradiSubmit}>
+            <Form.Group controlId="ime">
+                    <Form.Label>Ime</Form.Label>
+                    <Form.Control type="text" name="ime" required defaultValue={djelatnik.ime}/>
+                </Form.Group>
+
+                <Form.Group controlId="prezime">
+                    <Form.Label>Prezime</Form.Label>
+                    <Form.Control type="text" name="prezime" required  defaultValue={djelatnik.prezime}/>
+                </Form.Group>
+
+                <Form.Group controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="text" name="email" required  defaultValue={djelatnik.email}/>
+                </Form.Group>
+               
+                <hr />
+                <Row>
+                    <Col xs={6} sm={6} md={3} lg={6} xl={6} xxl={6}>
+                    <Link to={RoutesNames.DJELATNIK_PREGLED}
+                    className="btn btn-danger siroko">
+                    Odustani
+                    </Link>
+                    </Col>
+                    <Col xs={6} sm={6} md={9} lg={6} xl={6} xxl={6}>
+                    <Button variant="primary" type="submit" className="siroko">
+                        Promjeni djelatnika
+                    </Button>
+                    </Col>
+                </Row>
+            </Form>
+        </>
+    )
+
+
 
 
 }
