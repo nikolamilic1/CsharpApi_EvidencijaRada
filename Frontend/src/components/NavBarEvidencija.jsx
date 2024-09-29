dea549045a91284bdfa18974eb625d81c7debbde
Nikola Milić <nikk.mil@gmail.com>
2024-09-29 20:57:39 +0200

    Update NavBarEvidencija.jsx

--- Frontend/src/components/NavBarEvidencija.jsx
+++ Frontend/src/components/NavBarEvidencija.jsx
@@ -12,7 +12,7 @@ export default function NavBarEvidencija() {
     return(
         <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Evidencija rada</Navbar.Brand>
+        <Navbar.Brand href="/">Evidencija rada</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -20,9 +20,7 @@ export default function NavBarEvidencija() {
             <Nav.Link href="https://nikolamilic1-001-site1.htempurl.com/swagger/index.html" target='_blank'>Swagger</Nav.Link>
             <NavDropdown title="Programi" id="basic-nav-dropdown">
               <NavDropdown.Item onClick={()=>navigate(RoutesNames.PROJEKT_PREGLED)}>Projekti</NavDropdown.Item>
-              <NavDropdown.Item onClick={()=>navigate(RoutesNames.DJELATNIK_PREGLED)}>
-                Djelatnici
-              </NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>navigate(RoutesNames.DJELATNIK_PREGLED)}>Djelatnici</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
