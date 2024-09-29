93139f60077a350fa45def08b8a2a108dda8a73a
Nikola Milić <nikk.mil@gmail.com>
2024-09-29 20:57:52 +0200

    Update Constants.js

--- Frontend/src/Constants.js
+++ Frontend/src/Constants.js
@@ -5,10 +5,10 @@ PROJEKT_PREGLED: '/projekti',
 PROJEKT_NOVI: '/projekti/dodaj',
 PROJEKT_PROMJENA: '/projekti/:sifra',
 
-DJELATNIK_PREGLED: '/djelatnik',
-DJELATNIK_NOVI: '/djelatnik/dodaj',
-DJELATNIK_PROMJENA: '/djelatnik/:sifra'
+DJELATNIK_PREGLED: '/djelatnici',
+DJELATNIK_NOVI: '/djelatnici/dodaj',
+DJELATNIK_PROMJENA: '/djelatnici/:sifra'
 };
 
 // export const BECKEND_URL = 'https://nikolamilic1-001-site1.htempurl.com/api/v1';
-export const BECKEND_URL = 'https://localhost:5713/api/v1';
+export const BECKEND_URL = 'http://localhost:5173/api/v1';
