858b6bdea28c4de796e6cf236078721f67f8d311
Raguž Lea <57691651+nikolamilic1@users.noreply.github.com>
2024-10-01 19:02:59 +0200

    Update NavBarEvidencija.jsx

--- Frontend/src/components/NavBarEvidencija.jsx
+++ Frontend/src/components/NavBarEvidencija.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { RoutesNames } from '../Constants';
 
 export default function NavBarEvidencija() {
