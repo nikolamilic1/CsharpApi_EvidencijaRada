5b3f4240077bbbb5c37f95a6187e63673049c61f
Raguž Lea <57691651+nikolamilic1@users.noreply.github.com>
2024-10-01 19:03:02 +0200

    Update Constants.js

--- Frontend/src/Constants.js
+++ Frontend/src/Constants.js
@@ -10,5 +10,5 @@ DJELATNIK_NOVI: '/djelatnici/dodaj',
 DJELATNIK_PROMJENA: '/djelatnici/:sifra'
 };
 
-// export const BECKEND_URL = 'https://nikolamilic1-001-site1.htempurl.com/api/v1';
-export const BECKEND_URL = 'http://localhost:5173/api/v1';
+export const BECKEND_URL = 'https://nikolamilic1-001-site1.htempurl.com/api/v1';
+//export const BECKEND_URL = 'http://localhost:5173/api/v1';
