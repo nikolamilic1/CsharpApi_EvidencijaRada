171304d465fb552d7d35d60e8f29945e8a5f8f29
Raguž Lea <57691651+nikolamilic1@users.noreply.github.com>
2024-10-04 18:08:27 +0200

    Create ZadatakDTORead.cs

---
+++ Models/DTO/ZadatakDTORead.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpApi_EvidencijaRada.Models.DTO
+{
+    public record ZadatakDTORead(
+        int sifra,
+        DateTime? Pocetak,
+        DateTime? Zavrsetak,
+        string? ProjektNaziv,
+        string? Opis
+        );
+    
+}
