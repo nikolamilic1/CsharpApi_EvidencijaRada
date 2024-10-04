110584da6f519e96f4e529dae294dc981b890963
Nikola Milić <nikk.mil@gmail.com>
2024-10-04 13:04:31 +0200

    Create Zadatak.cs

---
+++ Models/Zadatak.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CsharpApi_EvidencijaRada.Models
+{
+    public class Zadatak:Entitet
+    {
+        public string? Naziv {  get; set; }
+
+        public DateTime? Pocetak {  get; set; }
+
+        public DateTime? Zavrsetak { get; set; }
+
+        [ForeignKey("projekt")]
+        public required Projekt Projekt { get; set; }
+
+        public string? Opis { get; set; }
+
+        public ICollection<Djelatnik> Djelatnik { get; set; }
+
+
+    }
+}
