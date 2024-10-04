66b0265ec62ed6f3a3ece51b2d6fdacd618a5468
Raguž Lea <57691651+nikolamilic1@users.noreply.github.com>
2024-10-04 18:08:24 +0200

    Create ZadatakDTOInsertUpdate.cs

---
+++ Models/DTO/ZadatakDTOInsertUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsharpApi_EvidencijaRada.Models.DTO
+{
+    public record ZadatakDTOInsertUpdate(
+                [Required(ErrorMessage = "Naziv obavezno")]
+                string? Naziv,
+              //  [Required(ErrorMessage = "Datum početka obavezno")]
+                DateTime? Pocetak,
+
+                DateTime? Zavrsetak,
+                [Range(1, int.MaxValue, ErrorMessage = "{0} mora biti između {1} i {2}")]
+                [Required(ErrorMessage = "Projekt obavezno")]
+                int ProjektSifra,
+
+                string? Opis
+
+
+        );
+
+
+}
