0e5a35710a5d9db79bc126fcade191f0dd73fbec
Nikola Milić <nikk.mil@gmail.com>
2024-10-05 13:55:41 +0200

    Frontend Zadatak  1:n

--- Frontend/src/pages/zadatci/ZadatciPromjena.jsx
+++ Frontend/src/pages/zadatci/ZadatciPromjena.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import ProjektService from "../../services/ProjektService";
 import { RoutesNames } from "../../Constants";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import Service from '../../services/ZadatakService';
 
 
 
@@ -43,13 +45,13 @@ export default function ZadaciPromjena() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
       },[]);
 
-      async function dodaj(e) {
-        const odgovor = await Service.dodaj(e);
+      async function promjena(e) {
+        const odgovor = await Service.promjena(routeParams.sifra,e);
     if(odgovor.greska){
       alert(odgovor.poruka);
       return;
     }
-    navigate(RoutesNames.DJELATNIK_PREGLED);
+    navigate(RoutesNames.PROJEKT_PREGLED);
       }
 
       function obradiSubmit(e) {
@@ -58,22 +60,75 @@ export default function ZadaciPromjena() {
         const podaci = new FormData(e.target);
 
 
-    dodaj({
+    promjena({
       naziv: podaci.get('naziv'),     
-      pocetak: parseInt(podaci.get('maksimalnopolaznika')),
-      zavrsetak: parseInt(podaci.get('maksimalnopolaznika')),
+      pocetak: parseInt(podaci.get('pocetak')),
+      zavrsetak: parseInt(podaci.get('zavrsetak')),
       projektSifra: parseInt(projektSifra),
       opis: podaci.get('opis')
     });
         
       }
+return (
+<>
+Mjenjanje podataka zadatka
 
+<Form onSubmit={obradiSubmit}>
+          <Form.Group controlId="naziv">
+              <Form.Label>Naziv</Form.Label>
+              <Form.Control type="text" name="naziv" required defaultValue={zadatak.naziv}/>
+          </Form.Group>
 
+          // pocetak   pogledati Smjerovi/SmjeroviPromjena za format datuma
+          <Form.Group controlId="pocetak">
+                    <Form.Label>Izvodi se od</Form.Label>
+                    <Form.Control type="date" name="pocetak"  
+                    defaultValue={zadatak.pocetak}/>
+                </Form.Group>
 
+          // zavrsetak   pogledati Smjerovi/SmjeroviPromjena za format datuma
+          <Form.Group controlId="zavrsetak">
+                    <Form.Label>Izvodi se od</Form.Label>
+                    <Form.Control type="date" name="zavrsetak"  
+                    defaultValue={zadatak.zavrsetak}/>
+                </Form.Group>
 
+          <Form.Group className='mb-3' controlId='projekt'>
+            <Form.Label>Projekt</Form.Label>
+            <Form.Select
+            value={projektSifra}
+            onChange={(e)=>{setProjektSifra(e.target.value)}}
+            >
+            {projekt && projekt.map((s,index)=>(
+              <option key={index} value={s.sifra}>
+                {s.naziv}
+              </option>
+            ))}
+            </Form.Select>
+          </Form.Group>
+                // mislim da ne treba required ispred defaultValue 
+          <Form.Group controlId="opis">
+              <Form.Label>Opis zadatka</Form.Label>
+              <Form.Control type="text" name="opis" required defaultValue={zadatak.opis}/>
+          </Form.Group>       
 
-
-
+          <hr />
+          <Row>
+              <Col xs={6} sm={6} md={3} lg={6} xl={6} xxl={6}>
+              <Link to={RoutesNames.ZADATAK_PREGLED}
+              className="btn btn-danger siroko">
+              Odustani
+              </Link>
+              </Col>
+              <Col xs={6} sm={6} md={9} lg={6} xl={6} xxl={6}>
+              <Button variant="primary" type="submit" className="siroko">
+                  Promjeni zadatak
+              </Button>
+              </Col>
+          </Row>
+      </Form>
+</>
+);
 
 
 
