088908040dfbebb305c716b17a7aa605479e80e8
Nikola Milić <nikk.mil@gmail.com>
2024-10-05 13:56:11 +0200

    Frontend Zadatak  1:n

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,9 @@ import ProjektiPromjena from './pages/projekti/ProjektiPromjena'
 import DjelatniciPregled from './pages/djelatnici/DjelatniciPregled'
 import DjelatniciDodaj from './pages/djelatnici/DjelatniciDodaj'
 import DjelatniciPromjena from './pages/djelatnici/DjeltniciPromjena'
+import ZadatakPregled from './pages/zadatci/ZadatciPregled'
+import ZadatciDodaj from './pages/zadatci/ZadatciDodaj'
+import ZadaciPromjena from './pages/zadatci/ZadatciPromjena'
 
 function App() {
 
@@ -26,6 +29,10 @@ function App() {
         <Route path={RoutesNames.DJELATNIK_PREGLED} element={<DjelatniciPregled />} />
         <Route path={RoutesNames.DJELATNIK_NOVI} element={<DjelatniciDodaj />} />
         <Route path={RoutesNames.DJELATNIK_PROMJENA} element={<DjelatniciPromjena />} />
+
+        <Route path={RoutesNames.ZADATAK_PREGLED} element={<ZadatakPregled />} />
+        <Route path={RoutesNames.ZADATAK_NOVI} element={<ZadatciDodaj />} />
+        <Route path={RoutesNames.ZADATAK_PROMJENA} element={<ZadaciPromjena />} />
       </Routes>
     </>
   )
