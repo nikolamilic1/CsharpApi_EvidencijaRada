e3fff208e118e19d977666696342a983abffcb04
Nikola Milić <nikk.mil@gmail.com>
2024-10-15 09:31:20 +0200

    Update ZadatakDTOInsertUpdate.cs

--- Models/DTO/ZadatakDTOInsertUpdate.cs
+++ Models/DTO/ZadatakDTOInsertUpdate.cs
@@ -5,12 +5,13 @@ namespace CsharpApi_EvidencijaRada.Models.DTO
     public record ZadatakDTOInsertUpdate(
                 [Required(ErrorMessage = "Naziv obavezno")]
                 string? Naziv,
-                 //[Required(ErrorMessage = "Datum početka obavezno")]
-                DateTime? Pocetak,
-                DateTime? Zavrsetak,
-                [Range(1, int.MaxValue, ErrorMessage = "{0} mora biti između {1} i {2}")]
-                [Required(ErrorMessage = "Projekt obavezno")]
-                int ProjektSifra,
+                [Required(ErrorMessage = "Datum početka obavezno")]
+                DateTime Pocetak,
+                [Required(ErrorMessage = "Datum završetka obavezno")]
+                DateTime Zavrsetak,
+                //[Range(1, int.MaxValue, ErrorMessage = "{0} mora biti između {1} i {2}")]
+                [Required(ErrorMessage = "Molimo odaberite projekt")]
+                int? ProjektSifra,
                 string? Opis
 
         );
