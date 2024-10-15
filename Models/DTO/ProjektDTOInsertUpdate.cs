6cce9e7035ac7eaa41dd4735314eebf63ffccd24
Nikola Milić <nikk.mil@gmail.com>
2024-10-15 16:08:05 +0200

    obrisan direktorij Frontend

--- Models/DTO/ProjektDTOInsertUpdate.cs
+++ Models/DTO/ProjektDTOInsertUpdate.cs
@@ -6,10 +6,6 @@ namespace CsharpApi_EvidencijaRada.Models.DTO
         [Required(ErrorMessage = "Naziv obavezno")]
         string Naziv,
         [Required(ErrorMessage = "Klijent obavezno")]
-        string Klijent,
-        DateTime Pocetak,
-        DateTime Zavrsetak,
-        int ProjektNaziv,
-        string Opis
+        string Klijent
         );
 }
