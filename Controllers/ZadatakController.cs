bb5e9adbece3b8aae942ed3eb3573736580a0d39
Nikola Milić <nikk.mil@gmail.com>
2024-10-21 22:37:42 +0200

    Update ZadatakController.cs

--- Controllers/ZadatakController.cs
+++ Controllers/ZadatakController.cs
@@ -188,7 +188,117 @@ namespace CsharpApi_EvidencijaRada.Controllers
             }
         }
 
+        [HttpGet]
+        [Route("Djelatnik/{sifraZadatak:int}")]
+        public ActionResult<List<DjelatnikDTORead>> GetDjelatnici(int sifraZadatak)
+        {
+            if (!ModelState.IsValid || sifraZadatak <= 0)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var p = _context.Zadatak
+                    .Include(i => i.Djelatnik).FirstOrDefault(x => x.Sifra == sifraZadatak);
+                if (p == null)
+                {
+                    return BadRequest("Ne postoji zadatak s šifrom " + sifraZadatak + " u bazi");
+                }
 
+                return Ok(_mapper.Map<List<DjelatnikDTORead>>(p.Djelatnik));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { poruka = ex.Message });
+            }
+        }
+        
+
+        [HttpPost]
+        [Route("{sifra:int}/dodaj/{djelatnikSifra:int}")]
+        public IActionResult DodajDjelatnika(int sifra, int djelatnikSifra)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (sifra <= 0 || djelatnikSifra <= 0)
+            {
+                return BadRequest("Šifra zadatka ili djelatnika nije dobra");
+            }
+            try
+            {
+                var zadatak = _context.Zadatak
+                    .Include(g => g.Djelatnik)
+                    .FirstOrDefault(g => g.Sifra == sifra);
+                if (zadatak == null)
+                {
+                    return BadRequest("Ne postoji zadatak s šifrom " + sifra + " u bazi");
+                }
+                var djelatnik = _context.Djelatnik.Find(djelatnikSifra);
+                if (djelatnik == null)
+                {
+                    return BadRequest("Ne postoji djelatnik s šifrom " + djelatnikSifra + " u bazi");
+                }
+                zadatak.Djelatnik.Add(djelatnik);
+                _context.Zadatak.Update(zadatak);
+                _context.SaveChanges();
+                return Ok(new
+                {
+                    poruka = "Djelatnik " + djelatnik.Prezime + " " + djelatnik.Ime + " dodan na grupu "
+                 + zadatak.Naziv
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                       StatusCodes.Status503ServiceUnavailable,
+                       ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{sifra:int}/obrisi/{djelatnikSifra:int}")]
+        public IActionResult ObrisiDjelatnika(int sifra, int djelatnikSifra)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (sifra <= 0 || djelatnikSifra <= 0)
+            {
+                return BadRequest("Šifra zadatka ili djelatnika nije dobra");
+            }
+            try
+            {
+                var zadatak = _context.Zadatak
+                    .Include(g => g.Djelatnik)
+                    .FirstOrDefault(g => g.Sifra == sifra);
+                if (zadatak == null)
+                {
+                    return BadRequest("Ne postoji zadatak s šifrom " + sifra + " u bazi");
+                }
+                var djelatnik = _context.Djelatnik.Find(djelatnikSifra);
+                if (djelatnik == null)
+                {
+                    return BadRequest("Ne postoji djelatnik s šifrom " + djelatnikSifra + " u bazi");
+                }
+                zadatak.Djelatnik.Remove(djelatnik);
+                _context.Zadatak.Update(zadatak);
+                _context.SaveChanges();
+
+                return Ok(new
+                {
+                    poruka = "Djelatnik " + djelatnik.Prezime + " " + djelatnik.Ime + " je obrisan sa zadatka "
+                 + zadatak.Naziv
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { poruka = ex.Message });
+
+            }
+        }
 
 
 
